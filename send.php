41129dc8528d821bce0b89ce833d22eb72a71dc7
ShailShrii <shailshrii2000@gmail.com>
2025-12-25 12:04:23 +0530

    Website updated

--- send.php
+++ send.php
@@ -1,45 +1,29 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // YOUR EMAIL (where enquiry will come)
-    $to = "ampolerope@gmail.com";
-
-    // GET FORM DATA (safe)
-    $name    = htmlspecialchars($_POST['name']);
-    $email   = htmlspecialchars($_POST['email']);
-    $mobile  = htmlspecialchars($_POST['mobile']);
-    $message = htmlspecialchars($_POST['message']);
-
-    // SUBJECT
-    $subject = "New Enquiry - AMPOLE Rope";
-
-    // EMAIL BODY
-    $body = "
-New Enquiry Received:
-
-Name: $name
-Email: $email
-Mobile: $mobile
-
-Message:
-$message
-    ";
-
-    // HEADERS
-    $headers  = "From: AMPOLE Rope <no-reply@ampolerope.com>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-
-    // SEND MAIL
-    if (mail($to, $subject, $body, $headers)) {
-        echo "<script>
-                alert('Thank you! Your enquiry has been sent.');
-                window.location.href='index.html';
-              </script>";
-    } else {
-        echo "<script>
-                alert('Error! Please try again later.');
-                window.history.back();
-              </script>";
-    }
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    exit('Method Not Allowed');
 }
-?>
+
+$name    = trim($_POST['name'] ?? '');
+$email   = trim($_POST['email'] ?? '');
+$mobile  = trim($_POST['mobile'] ?? '');
+$message = trim($_POST['message'] ?? '');
+
+$to = "virenbhatt@ampolerope.com";
+$subject = "New Enquiry - AMPOLE Rope";
+
+$body = "New Enquiry\n\n";
+$body .= "Name: $name\n";
+$body .= "Email: $email\n";
+$body .= "Mobile: $mobile\n\n";
+$body .= "Message:\n$message";
+
+$headers  = "From: AMPOLE Rope <no-reply@".$_SERVER['SERVER_NAME'].">\r\n";
+$headers .= "Reply-To: $email\r\n";
+
+@mail($to, $subject, $body, $headers);
+
+echo "<script>
+alert('Thank you! Your enquiry has been sent.');
+window.location.href='index.html';
+</script>";
